638cc2a714aeb7172b6878bb37638325ce81aae3
root <ukiran75@gmail.com>
2017-04-19 22:20:35 +0000

    MOTD update

--- cookbooks/MOTD/recipes/default.rb
+++ cookbooks/MOTD/recipes/default.rb
@@ -4,7 +4,13 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
-template "/etc/motd" do
-	source "motd.erb"
-	mode "0644"
+#template "/etc/motd" do
+#	source "motd.erb"
+#	mode "0644"
+#end
+
+host_name = node['hostname']
+
+file '/etc/motd' do
+	content "Hostname is this : #{host_name}"
 end
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,8 +5,14 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 # install apache package
 
+if node['platform_family'] == "rhel"
+	package = "httpd"
+elsif node['platform_family'] == "debian"
+	package = "apache2"
+end
+
 package "apache2" do
-	package_name 'httpd'
+	package_name package
 	action :install
 end
 
@@ -14,3 +20,5 @@ service "apache2" do
 	service_name 'httpd'
 	action [:enable, :start]
 end
+
+include_recipe 'apache::websites'
